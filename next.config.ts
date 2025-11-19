0517914f866a4daff8fcfe4dfa8163b04c1113a2
Nicola Cortinovis <nicolacortinovis@Mac.station>
2025-11-19 10:03:57 +0100

    install required deps

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
   },
 };
 
