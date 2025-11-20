9e30454b909a8255f54f677ac997a34d0174e021
Nicola Cortinovis <nicolacortinovis@Mac.station>
2025-11-20 23:27:18 +0100

    Update header, cursor, blend mode, lenis

---
+++ app/providers.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      lerp: 0.08,
+      wheelMultiplier: 0.9,
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
+  return <>{children}</>;
+}
