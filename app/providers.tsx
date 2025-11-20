a1a94fa8eb68350f034cc05cfa5fbe6597094156
Nicola Cortinovis <nicolacortinovis@Mac.station>
2025-11-20 23:49:45 +0100

    update layout + header + styles

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,26 +1,22 @@
 "use client";
 
-import { useEffect } from "react";
+import { ReactNode, useEffect } from "react";
 import Lenis from "lenis";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({ children }: { children: ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
-      lerp: 0.08,
-      wheelMultiplier: 0.9,
-      duration: 1.2,
+      lerp: 0.1,
+      wheelMultiplier: 1.1,
     });
 
     function raf(time: number) {
       lenis.raf(time);
       requestAnimationFrame(raf);
     }
-
     requestAnimationFrame(raf);
 
-    return () => {
-      lenis.destroy();
-    };
+    return () => lenis.destroy();
   }, []);
 
   return <>{children}</>;
