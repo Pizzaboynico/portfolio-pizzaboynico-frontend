5705d3daa05ba4151df788a1018a44b0a752f78e
Nicola Cortinovis <nicolacortinovis@Mac.station>
2025-11-20 20:21:11 +0100

    Fix: Rigenenerazione package-lock e pulizia dipendenze per deploy

--- types/project.ts
+++ types/project.ts
@@ -1,6 +1,16 @@
+import { SanityImageSource } from "next-sanity-image";
+
+/**
+ * Definisce la struttura dei dati di un singolo progetto 
+ * recuperato da Sanity.
+ */
 export interface Project {
   _id: string;
   title: string;
-  category?: string;
-  mainImage: any;
+  slug: {
+    current: string;
+  };
+  // L'immagine principale, usiamo il tipo specifico di next-sanity-image
+  mainImage: SanityImageSource; 
+  description?: string;
 }
