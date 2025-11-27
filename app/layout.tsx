6154311a1b5257986ab8aa1d91414304f1b6c4b3
Nicola Cortinovis <nicolacortinovis@Mac.station>
2025-11-27 15:45:28 +0100

    Revert: Remove List View and Filters, restore Grid View

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import "@/styles/grid.css";
 import "@/styles/modal.css";
 import "@/styles/header.css";
-import "@/styles/filters.css";
 import ClientLayout from "@/components/ClientLayout";
 
 export const metadata: Metadata = {
