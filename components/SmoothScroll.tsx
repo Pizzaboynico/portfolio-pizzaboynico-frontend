f3e97f89f3c25355e9c2d21f432ea97b730d86dc
Nicola Cortinovis <nicolacortinovis@Mac.station>
2025-12-01 17:18:31 +0100

    Fix mobile scroll: re-enable Lenis with mobile optimization

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -3,6 +3,29 @@ import { useEffect } from "react";
 import Lenis from "lenis";
 
 export default function SmoothScroll() {
-    // Lenis disabled - using native scroll for better performance and stability
+    useEffect(() => {
+        const lenis = new Lenis({
+            duration: 1.2,
+            easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            direction: 'vertical',
+            gestureDirection: 'vertical',
+            smooth: true,
+            mouseMultiplier: 1,
+            smoothTouch: false,
+            touchMultiplier: 2,
+        } as any);
+
+        function raf(time: number) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+
+        requestAnimationFrame(raf);
+
+        return () => {
+            lenis.destroy();
+        };
+    }, []);
+
     return null;
 }
